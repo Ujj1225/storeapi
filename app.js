e15fce62033d047cfc1cbd83756c29f191172e02
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-13 20:52:52 +0545

    Random

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('04 Store API')
+require("dotenv").config();
+
+// Async errors
+
+const express = require("express");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1> <a href= "/api/v1/products">products route</a>');
+});
+
+// Products route
+
+app.use(errorHandlerMiddleware);
+app.use(notFound);
