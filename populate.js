b56f928beb321e51aceacb3ae6f7c0d74ec70c94
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-14 10:41:06 +0545

    Setting up to populate data directly from products.json

---
+++ populate.js
@@ -0,0 +1,21 @@
+// Insted of adding all data to database one by one. What we do is simply populate the data from the products.json
+require("dotenv").config();
+
+// Cating up model and connection for db
+const connectDB = require("./db/connect");
+// Product was the name of the model we defined
+const Product = require("./models/product");
+
+const jsonProducts = require("./products.json");
+
+const start = async () =>
+{
+    try {
+        await connectDB(process.env.MONGO_URL);
+        console.log('connection successful');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
