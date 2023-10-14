2c9954a17b4789ffd655514be835a893552c3739
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-14 19:47:45 +0545

    Filtering function using find method for product

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,14 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   // throw new Error('testing async errors');
-  const products = await Product.find({});
-  res.status(200).json({ products });
+
+  // The .find({}) finds all the products
+  // Any property of the model inside .find will be found like here .find({featured: true})
+  const products = await Product.find({
+    featured: true,
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = (req, res) => {
