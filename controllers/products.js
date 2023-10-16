7fa0f6c4d7cefce7f0dead6345130df05db67bee
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-17 03:25:27 +0545

    Setting up basic sorting using req.query

--- controllers/products.js
+++ controllers/products.js
@@ -7,12 +7,24 @@ const getAllProductsStatic = async (req, res) => {
   // Any property of the model inside .find will be found like here .find({featured: true})
   const products = await Product.find({
     featured: true,
+    name: "vase table",
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).json({ message: "products route" });
+const getAllProducts = async (req, res) => {
+  try {
+    const { featured } = req.query;
+    const queryObject = {};
+    if (featured) {
+      queryObject.featured = featured === "true" ? true : false;
+    }
+    console.log(queryObject);
+    const products = await Product.find(queryObject);
+    res.status(200).json({ products, nbHits: products.length });
+  } catch (error) {
+    res.status(500).json({ error: "An error occured!" });
+  }
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
