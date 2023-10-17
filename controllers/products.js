ca4437b76fe91e2e35f60f6431217657edbaf581
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-18 04:53:04 +0545

    Added page, limit and skip onto our api

--- controllers/products.js
+++ controllers/products.js
@@ -27,7 +27,8 @@ const getAllProducts = async (req, res) => {
   // sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
-    console.log(sortList); //This prints the request like -name price
+    // console.log(sortList); 
+    //This prints the request like -name price
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
@@ -38,6 +39,14 @@ const getAllProducts = async (req, res) => {
     // .select is used to select specific fields
     result = result.select(filedList);
   }
+  // req.query always returns a string like {featured: 'false', page: '2'}
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page -1 ) * limit;
+  result = result.skip(skip).limit(limit)
+  // Total items; 23
+  // now if limit is 7 then we will have 4 pages namely: 7 7 7 2
+  // so for second page: page = 2 hence first 7 items will be skipped
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
