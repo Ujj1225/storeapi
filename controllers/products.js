dcc1d59d1dc1606f5be4771140bd58d679826145
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-18 04:08:57 +0545

    Added sorting functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,37 +1,40 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "a";
-  // throw new Error('testing async errors');
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
+    .select("name price");
 
-  // The .find({}) finds all the products
-  // Any property of the model inside .find will be found like here .find({featured: true})
-  const products = await Product.find({
-    // Regular expression or regex is used to describe a pattern and here option: i simply means case insensivite
-    name: { $regex: search, $options: "i" },
-  });
+  res.status(200).json({ products, nbHits: products.length });
+};
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  let result = Product.find(queryObject);
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    console.log(sortList); //This prints the request like -name price
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
-const getAllProducts = async (req, res) => {
-  try {
-    const { featured, company, name } = req.query;
-    const queryObject = {};
-    if (featured) {
-      queryObject.featured = featured === "true" ? true : false;
-    }
-    if (company) {
-      queryObject.company = company;
-    }
-    if (name) {
-      queryObject.name = {$regex: name, $options: 'i'}
-    }
-    console.log(queryObject);
-    const products = await Product.find(queryObject);
-    res.status(200).json({ products, nbHits: products.length });
-  } catch (error) {
-    res.status(500).json({ error: "An error occured!" });
-  }
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
 };
-
-module.exports = { getAllProducts, getAllProductsStatic };
