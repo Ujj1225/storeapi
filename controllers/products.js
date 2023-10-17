3b5295c21c2552dbd5fb1d6ec8dd30a78efc1901
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-10-18 05:08:12 +0545

    Setting up for numericFilters

--- controllers/products.js
+++ controllers/products.js
@@ -2,15 +2,15 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({ price: { $gt: 30 } })
-    .sort("name")
+    .sort("price")
     .select("name price")
     .limit(10)
-    .skip(5);
+    .skip(0);
 
   res.status(200).json({ nbHits: products.length, products });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -22,12 +22,16 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+  if (numericFilters) {
+    console.log(numericFilters);
+  }
+  console.log(queryObject);
 
   let result = Product.find(queryObject);
   // sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
-    // console.log(sortList); 
+    // console.log(sortList);
     //This prints the request like -name price
     result = result.sort(sortList);
   } else {
@@ -40,10 +44,10 @@ const getAllProducts = async (req, res) => {
     result = result.select(filedList);
   }
   // req.query always returns a string like {featured: 'false', page: '2'}
-  const page = Number(req.query.page) || 1
-  const limit = Number(req.query.limit) || 10
-  const skip = (page -1 ) * limit;
-  result = result.skip(skip).limit(limit)
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
   // Total items; 23
   // now if limit is 7 then we will have 4 pages namely: 7 7 7 2
   // so for second page: page = 2 hence first 7 items will be skipped
